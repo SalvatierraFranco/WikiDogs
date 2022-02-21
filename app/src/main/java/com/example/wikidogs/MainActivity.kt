d238d302cc800372b19605bd0f478e4391a64c82
Franco <franco.salvatierradvp@gmail.com>
2022-02-21 13:22:03 -0300

    Codigo para configurar recyclerview y obtener los datos de la API

--- app/src/main/java/com/example/wikidogs/MainActivity.kt
+++ app/src/main/java/com/example/wikidogs/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.wikidogs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.wikidogs.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -12,5 +17,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    private fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    private fun searchByBreed(query: String){
+        CoroutineScope(Dispatchers.IO).launch {
+            var call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+
+            var puppies = call.body()
+            if(call.isSuccessful){
+
+            }else{
+
+            }
+        }
     }
 }
