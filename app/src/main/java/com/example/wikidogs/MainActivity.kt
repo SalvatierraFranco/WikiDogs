2b91866fbbd071d128d01f90529f1039647b6a47
Franco <franco.salvatierradvp@gmail.com>
2022-02-21 13:52:00 -0300

    Codigo UI de RecyclerView terminado

--- app/src/main/java/com/example/wikidogs/MainActivity.kt
+++ app/src/main/java/com/example/wikidogs/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.wikidogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wikidogs.databinding.ActivityMainBinding
+import com.example.wikidogs.recyclerview.DogAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -11,12 +14,20 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: DogAdapter
+    private var dogImages: MutableList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initRecyclerView()
+    }
 
+    private fun initRecyclerView(){
+        adapter = DogAdapter(dogImages)
+        binding.rvDogs.layoutManager = LinearLayoutManager(this)
+        binding.rvDogs.adapter = adapter
     }
 
     private fun getRetrofit(): Retrofit {
@@ -31,11 +42,17 @@ class MainActivity : AppCompatActivity() {
             var call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
 
             var puppies = call.body()
-            if(call.isSuccessful){
-
-            }else{
-
+            runOnUiThread {
+                if(call.isSuccessful){
+                    val images = puppies?.images ?: emptyList()
+                    dogImages.clear()
+                    dogImages.addAll(images)
+                    adapter.notifyDataSetChanged()
+                }else{
+                    Toast.makeText(this@MainActivity, "Ha ocurrido un error", Toast.LENGTH_LONG).show()
+                }
             }
+
         }
     }
 }
